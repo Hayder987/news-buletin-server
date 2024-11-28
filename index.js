2226483c055e49ec03d95bba566eb86a013bc8e8
hayder <hayderbd4290@gmail.com>
2024-11-28 18:03:03 +0600

    successfully conected mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 4000;
 const app = express();
+require('dotenv').config()
 
 app.use(express.json());
 app.use(cors())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.jheyv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+async function run() {
+  try {
+    await client.connect();
+
+
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+   
+  }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res)=>{
     res.send('hello world')
